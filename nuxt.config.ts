74042d4e75b1f2a8c02448462791894db82cb5d1
congtuhaixinhzai <congtuhaixinhzai@users.noreply.github.com>
2026-08-27 18:08:15 +0700

    Fix GitHub Actions permissions and Nuxt baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/congtuhaixinhzai/'
+  },
   extends: ['shadcn-docs-nuxt'],
   compatibilityDate: '2024-07-06',
 });
